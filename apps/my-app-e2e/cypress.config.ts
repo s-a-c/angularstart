8250e3749e027576b8ec6709ffaaf698c57a2c7e
StandAloneComplex <71233932+s-a-c@users.noreply.github.com>
2024-12-18 12:38:00 +0000

    Initial commit
    To connect your workspace to Nx Cloud, push your repository
    to your git hosting provider and go to the following URL:
    
    https://cloud.nx.app/connect/nXBKeOJusa

---
+++ apps/my-app-e2e/cypress.config.ts
@@ -0,0 +1,8 @@
+import { nxE2EPreset } from '@nx/cypress/plugins/cypress-preset';
+    
+    import { defineConfig } from 'cypress';
+
+export default defineConfig({
+  e2e: { ...nxE2EPreset(__filename, {"cypressDir":"src","webServerCommands":{"default":"pnpm exec nx run my-app:serve","production":"pnpm exec nx run my-app:serve-static"},"ciWebServerCommand":"pnpm exec nx run my-app:serve-static","ciBaseUrl":"http://localhost:4200"}),
+baseUrl: 'http://localhost:4200' }
+});
